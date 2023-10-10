62f8fd2a0efdcdc19b60a6ccfeb1998c3ebcb0cd
Jacob Nelson <scoutnelson132@gmail.com>
2023-10-10 18:53:39 -0400

    updating state table, removing search and sort

--- static/scripts.js
+++ static/scripts.js
@@ -1,137 +1,52 @@
-function sortTable1() {
-  document.addEventListener('click', function(event) {
-      if (document.getElementsByClassName('table-sort')) {
-          const table = event.target.closest('table');
-          const index = event.target.cellIndex;
 
-          const rows = Array.from(table.querySelectorAll('tbody tr'));
-          const isAscending = event.target.classList.contains('asc');
-
-          rows.sort((a, b) => {
-              const aText = a.cells[index].textContent.trim();
-              const bText = b.cells[index].textContent.trim();
-
-              return (isAscending ? aText.localeCompare(bText) : bText.localeCompare(aText));
-          });
-
-          table.querySelector('thead tr').querySelectorAll('th').forEach(th => th.classList.remove('asc', 'desc'));
-          event.target.classList.toggle('asc', !isAscending);
-          event.target.classList.toggle('desc', isAscending);
-
-          rows.forEach(row => table.querySelector('tbody').appendChild(row));
-      }
-  });
-};
-
-
-
-
-
-function sortTables(n) {
+// TODO: troubleshoot asc and desc sorting
+function sortTable(n) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("land-area");
+  table = document.getElementById("state-area");
   switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
   while (switching) {
-    // Start by saying: no switching is done:
+    //start by saying: no switching is done:
     switching = false;
     rows = table.rows;
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
     for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
+      //start by saying there should be no switching:
       shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[n];
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementByClass("TD")[n];
       y = rows[i + 1].getElementsByTagName("TD")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
       if (dir == "asc") {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
           break;
         }
       } else if (dir == "desc") {
         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
+          //if so, mark as a switch and break the loop:
           shouldSwitch = true;
           break;
         }
       }
     }
     if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
     } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
-
-
-
-// start sorting land area table on homepage
-function sortLandTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("land-area");
-  switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++;
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
       if (switchcount == 0 && dir == "asc") {
         dir = "desc";
         switching = true;
@@ -142,11 +57,11 @@ function sortLandTable(n) {
 // end sorting land area table on homepage
 
 // start search for land-area table
-function stateLandSearch() {
+function stateSearch() {
   var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("state_size");
+  input = document.getElementById("state_search");
   filter = input.value.toUpperCase();
-  table = document.getElementById("land-area");
+  table = document.getElementById("state-area");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
@@ -220,7 +135,7 @@ function sortPopTable(n) {
 // end sorting land area table on homepage
 
 // start search for land-area table
-function statePopSearch() {
+function stateSearch() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("state_size");
   filter = input.value.toUpperCase();
